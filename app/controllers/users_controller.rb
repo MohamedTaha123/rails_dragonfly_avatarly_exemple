65c2727236100106498d15a6658555e2a8e564ba
Viktor Leonets <4405511@gmail.com>
2016-03-10 10:25:38 +0200

    added paperclip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name)
+      params.require(:user).permit(:first_name, :last_name, :avatar)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  has_attached_file :avatar, styles: { thumb: '250x250#' }
+
+  validates :first_name, presence: true
+  validates_attachment :avatar,
+    content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
